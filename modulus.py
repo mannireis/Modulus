1913414c0d99b720f169414a80f99875f3451e1a
manni <90857553+mannireis@users.noreply.github.com>
2025-05-03 18:57:04 +0100

    GOT MINECRAFT TO OPEN!!!!

--- modulus.py
+++ modulus.py
@@ -2,24 +2,37 @@ from PyQt5 import QtWidgets
 import sys
 import subprocess
 import minecraft_launcher_lib
+import os
 
+# Define the path to the natives (LWJGL libraries)
 natives_directory = f".minecraft/versions/1.21.1/natives"
 minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
-version = "1.21.1"  
+version = "1.21.1"
 
+# Install the Minecraft version if not already installed
 minecraft_launcher_lib.install.install_minecraft_version(version, minecraft_directory)
 
 options = {
-    "username": "Player", 
+    "username": "Player",
     "uuid": "00000000-0000-0000-0000-000000000000",
     "token": "",
-    "executablePath": "/usr/bin/java"  
+    "executablePath": "/usr/bin/java",
+    # Force ARM64 LWJGL and disable Wayland
+    "jvmArguments": [
+        "-Dorg.lwjgl.librarypath=" + os.path.join(minecraft_directory, "versions", version, "natives"),
+        "-Dorg.lwjgl.util.Debug=true",  # Debug LWJGL loading
+        "-Dorg.lwjgl.util.DebugLoader=true",
+        "-Djava.awt.headless=true",  # Avoid GUI issues
+    ],
 }
-
 
 
 # Get the Minecraft launch command
 minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version, minecraft_directory, options)
+
+# Add the Java library path argument for LWJGL (adjust the path if necessary)
+lwjgl_path = os.path.join(minecraft_directory, "versions", version, "natives")
+minecraft_command[2:2] = ["-Djava.library.path=" + lwjgl_path]
 
 class LauncherWindow(QtWidgets.QWidget):
     def __init__(self):
@@ -45,3 +58,6 @@ app = QtWidgets.QApplication(sys.argv)
 window = LauncherWindow()
 window.show()
 sys.exit(app.exec_())
+
+
+
